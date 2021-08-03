8b36047eb41c802b508eaf9a75e9a17ce78f1957
adriano <adriano08andrade@hotmail.com>
2021-08-03 19:15:40 -0300

    mudando debug para true

--- ISA/settings.py
+++ ISA/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9jh41k1e(+b!ndxw-7ht4i!8id7l-aak$1#&43n8l(oc0-f60%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
